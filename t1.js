171c61a0308efa8163a25af89246b7d80d226908
Volod1200sh <98735553+Volod1200sh@users.noreply.github.com>
2023-06-10 21:39:03 +0300

    Update t1.js

--- t1.js
+++ t1.js
@@ -1,5 +1,5 @@
 const filter_list = list => list.filter(x => typeof x !== 'string')
 
 console.log("[1,2,'a','b'] -", filter_list([1, 2, 'a', 'b']))
-console.log("[1,'a','b',0,15] -", filter_list([1, 'a', 'b', 0, 15]))
-console.log("[1,2,'aasf','1','123',123] -", filter_list([1, 2, 'aasf', '1', '123', 123]))
+console.log("[1,'a','b',0,30] -", filter_list([1, 'a', 'b', 0, 30]))
+console.log("[1,2,'aes','2','85',85] -", filter_list([1, 2, 'aes', '2', '85', 85]))
